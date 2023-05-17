08b8bb1f767cd39820f040ca92e6bf0a7aa97e07
calebadey <107336978+calebadey@users.noreply.github.com>
2023-05-17 22:29:13 +0100

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,16 @@
+// var a = window.prompt("Enter age: ")
+// console.log(a);
+
+// var a = window.prompt("ENTER AGE: ");
+// a = parseInt (a, 10);
+// if a(a<10){
+//     console.log("welcome participant, you have enered the number: " + a);
+// }
+
+// var x = 10;
+// var color = x > 20? 'orange' : 'blue';
+// console.log(color);
+
+let score = window.prompt("Enter your Name: ")
+score = parseInt;
+switch()
